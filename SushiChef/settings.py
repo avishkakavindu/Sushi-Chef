7c48d47a2b92eab618f7819cd9240d26836449e0
avishkakavindu <avishkakavindud@gmail.com>
2020-10-13 13:31:12 +0530

    Database configured

--- SushiChef/settings.py
+++ SushiChef/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SushiChef.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sushichefdb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
