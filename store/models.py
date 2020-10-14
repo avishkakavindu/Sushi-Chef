5e03469222721c1aa16d9bbddadb968598531ed0
avishkakavindu <avishkakavindud@gmail.com>
2020-10-14 19:13:41 +0530

    Models configured

--- store/models.py
+++ store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=4, decimal_places=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Chef(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.name
