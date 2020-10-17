228b25b481aa0d2793d65df73b434d802c2a3a18
avishkakavindu <avishkakavindud@gmail.com>
2020-10-17 13:32:09 +0530

    Promopage data rendering Done

--- store/models.py
+++ store/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     desc = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.DecimalField(max_digits=2, decimal_places=0)
+    updated_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
