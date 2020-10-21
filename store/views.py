5b71e5f10d8aafb8996591c97c57ef2ff65f6065
avishkakavindu <avishkakavindud@gmail.com>
2020-10-21 12:09:11 +0530

    Product detail js integration done

--- store/views.py
+++ store/views.py
@@ -74,7 +74,12 @@ def checkout(request):
 # product detail page
 def product_detail(request, product_id):
     product_detail = get_object_or_404(Product, pk=product_id)
+    images = get_object_or_404(Product, pk=product_id)
+
     context = {
-        'product': product_detail
+        'product': product_detail,
+        'main_img': images.productimage_set.filter(place="Main Product Image").all(),
+        'sub_img1': images.productimage_set.filter(place="Sub Image 1").all(),
+        'sub_img2': images.productimage_set.filter(place="Sub Image 2").all(),
     }
     return render(request, 'store/product.html', context)
