20ef00a2d0cbee05ed3eeb2b350e5ddcbcb3375d
avishkakavindu <avishkakavindud@gmail.com>
2020-11-03 18:48:24 +0530

    Role based permission decorator added

--- store/views/checkout.py
+++ store/views/checkout.py
@@ -6,7 +6,6 @@ from store.decorators import allowed_user
 
 # checkout page
 @login_required(login_url='/login')
-@allowed_user(allowed_roles=['admin'])
 def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
