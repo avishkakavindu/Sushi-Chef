06d1abed0284428adeb5d5057bf7e38742efc3ef
avishkakavindu <avishkakavindud@gmail.com>
2020-11-03 18:36:05 +0530

    Role based permission decorator added

--- store/decorators.py
+++ store/decorators.py
@@ -9,4 +9,13 @@ def unauthenticated_user(views_func):
             return redirect('/')
         else:
             return views_func(request, *args, **kwargs)
+
     return wrapper_func
+
+# allowed user filtration
+def allowed_user(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            return view_func(request, *args, **kwargs)
+        return wrapper_func
+    return decorator()
--- store/views/register.py
+++ store/views/register.py
@@ -10,24 +10,21 @@ from store.decorators import unauthenticated_user
 
 @unauthenticated_user
 def register(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    else:
-        form = CreateUserForm()
+    form = CreateUserForm()
 
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for {}. Now you can sign in.'.format(user))
-                return redirect('login')
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for {}. Now you can sign in.'.format(user))
+            return redirect('login')
 
-            else:
-                messages.error(request, "Something went wrong. Account not created!")
+        else:
+            messages.error(request, "Something went wrong. Account not created!")
 
-        context = {
-            'registration_form': form,
-        }
-        return render(request, 'registration/signup.html', context)
+    context = {
+        'registration_form': form,
+    }
+    return render(request, 'registration/signup.html', context)
 
